1a3ee1fb35f20d80a1590268a0c6be2d71e7292f
Niranjan kumar <167510174+Niranjankumar6108@users.noreply.github.com>
2025-03-27 19:34:48 +0000

    comparision and datatype summery

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,51 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2">1); // out-true
+// console.log("2">1); // out-true
+
+console.log(null > 0); // false 
+console.log(null == 0); // false 
+console.log(null >= 0); // out-true
+
+// ===
+
+console.log("2" === 2);
+
+// Premitive
+
+// 7 type: String,Number,Boolean,null,underfined,symboll,BigInt
+
+const score = 100
+const scoreValie = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+console.log(id == anotherId) // otp-false
+
+const bigNumber = 345646487246445n
+
+let userEmail;//undefind
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// Reference (Non premitive)
+
+// Array , Object, Function
+
+const arr = ["shaktiman", "naagraj", "doga"]
+
+let myObj = {
+    name:"Niranjan",
+    age:20,
+}
+
+const muFunctio = function(){
+    console.log("Hello world");
+}
